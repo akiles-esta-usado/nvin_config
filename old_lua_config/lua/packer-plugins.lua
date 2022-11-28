e8a3f36e2d32515ba7666e40ec2a34bbc60a54b3
Aquiles Viza <aquiles.viza@sansano.usm.cl>
2022-11-27 21:53:53 -0300

    First commit, added Powershell as terminal shell

---
+++ old_lua_config/lua/packer-plugins.lua
@@ -0,0 +1,5 @@
+return require("packer").startup(function()
+	use 'wbthomason/packer.nvim'
+	use 'gruvbox-community/gruvbox'
+	use 'vimwiki/vimwiki'
+end)
---
+++ old_lua_config/plugin/vimwiki.lua
@@ -0,0 +1,9 @@
+vim.g.vimwiki_hl_headers = 1
+
+extensions = { }
+extensions[".md"] = "markdown"
+
+vim.g.vimwiki_ext2syntax = extensions
+vim.g.vimwiki_url_maxsave = 0
+vim.g.vimwiki_conceallevel = 0
+vim.g.vimwiki_conceal_onechar_markers = 0
